0904b83dcd16b552733e50801aca5f80666c019b
ilnas <genatula@protonmail.com>
2021-12-22 15:24:23 +0300

    Fix alignment

--- hw5/src/ru/kpfu/itis/genatulin/genatulin/AppFrame.java
+++ hw5/src/ru/kpfu/itis/genatulin/genatulin/AppFrame.java
@@ -39,7 +39,7 @@ public class AppFrame extends JFrame {
     private void createRightPanel() {
         rightPanel.setOpaque(true);
         rightPanel.setBackground(new Color(243, 243, 243));
-        rightPanel.setPreferredSize(new Dimension(300, 500));
+        rightPanel.setPreferredSize(new Dimension(400, 500));
         rightPanel.setLayout(new BoxLayout(rightPanel, BoxLayout.Y_AXIS));
         rightPanel.setBorder(BorderFactory.createMatteBorder(0, 3, 0, 0, new Color(194, 194, 194)));
         createFacePartButtons(rightPanel);
@@ -55,12 +55,26 @@ public class AppFrame extends JFrame {
             JLabel label = new JLabel("Change " + facePart.toString().toLowerCase(Locale.ROOT) + ":");
             FacePartSwitchButton backButton = new FacePartSwitchButton(-1, facePart, "Back");
             FacePartSwitchButton forwardButton = new FacePartSwitchButton(1, facePart, "Next");
+            label.setBackground(new Color(243, 243, 243));
 
-            panel.add(label);
-            panel.add(Box.createRigidArea(new Dimension(10, 0)));
-            panel.add(backButton);
-            panel.add(Box.createRigidArea(new Dimension(10, 0)));
-            panel.add(forwardButton);
+            JPanel labelPanel = new JPanel();
+            labelPanel.setAlignmentX(Component.LEFT_ALIGNMENT);
+            labelPanel.setPreferredSize(new Dimension(20, 3));
+            labelPanel.setBackground(new Color(243, 243, 243));
+
+            JPanel buttonsPanel = new JPanel();
+            buttonsPanel.setAlignmentX(Component.RIGHT_ALIGNMENT);
+            buttonsPanel.setMinimumSize(new Dimension(15, 3));
+            buttonsPanel.setBackground(new Color(243, 243, 243));
+
+            labelPanel.add(label);
+            buttonsPanel.add(Box.createRigidArea(new Dimension(10, 0)));
+            buttonsPanel.add(backButton);
+            buttonsPanel.add(Box.createRigidArea(new Dimension(10, 0)));
+            buttonsPanel.add(forwardButton);
+
+            panel.add(labelPanel);
+            panel.add(buttonsPanel);
 
             container.add(Box.createRigidArea(new Dimension(0, 20)));
             container.add(panel);
@@ -70,7 +84,7 @@ public class AppFrame extends JFrame {
     private void createLabel() {
         label.setOpaque(true);
         label.setBackground(new Color(243, 243, 243));
-        label.setMinimumSize(new Dimension(400, 500));
+        label.setMinimumSize(new Dimension(350, 500));
         container.add(label, BorderLayout.WEST);
     }
 
